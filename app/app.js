5613d98c648f7370342d301ee57d148c40a7c4e4
Zhihao Liu <liu.zhihao@outlook.com>
2018-03-04 13:05:48 +0800

    Initialize

---
+++ app/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const path = require('path');
+const config = require('./config.json');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+  const mongo = await MongoClient.connect(config.mongoConnectionUrl);
+  const col = mongo.db('obor').collection('countries');
+  const docs = await col.find().toArray();
+  await mongo.close();
+  res.locals.docs = docs;
+  res.render('index');
+});
+
+app.listen(config.listeningPort);
+
+console.log(`Server listening on Port ${config.listeningPort}`);
---
+++ scripts/mongo-import/main.js
@@ -0,0 +1,20 @@
+const Mongo = require('mongodb').MongoClient;
+const dataProps = require('../raw-data/country-properties.json');
+const dataGeo = require('../raw-data/world-geo.json').features;
+
+(async () => {
+  const mongo = await Mongo.connect('mongodb://localhost:27017');
+  const col = mongo.db('obor').collection('countries');
+
+  await col.createIndex({ 'prop.countryId': 1 }, { unique: true });
+
+  for (let prop of dataProps) {
+    let geo = dataGeo.find(elem => elem.id == prop.countryId);
+    if (geo === undefined) geo = null;
+
+    const record = { prop: prop, geo: geo };
+    await col.updateOne({ 'prop.countryId': prop.countryId }, { $set: record }, { upsert: true });
+  }
+
+  console.log('DONE');
+})();
